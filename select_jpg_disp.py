337b89edf6c76be46359466295ee3b091f52b388
snc29654 <snc29654@nifty.com>
2021-10-15 20:52:32 +0900

    第2画面をクラスとする

--- select_jpg_disp.py
+++ select_jpg_disp.py
@@ -108,116 +108,116 @@ class image_gui():
     def quit(self):
         root.destroy()
 
-suspend_flag = 0
-def suspend():
-    global suspend_flag
-    suspend_flag = 1
-def resume():
-    global suspend_flag
-    suspend_flag = 0
+class sub_gui():
+    def __init__(self):
+        self.suspend_flag = 0
+    def suspend(self):
+        self.suspend_flag = 1
+    def resume(self):
+        self.suspend_flag = 0
 
-def speedup():
-    global interval
-    if(float(interval) > 0.1):
-        interval = float(interval) - 0.1
+    def speedup(self):
+        global interval
+        if(float(interval) > 0.1):
+            interval = float(interval) - 0.1
 
-def speeddown():
-    global interval
-    interval = float(interval) + 0.1
-
-
-def sizeup():
-    global sizerate
-    sizerate = float(sizerate) + 0.1
-
-def sizedown():
-    global sizerate
-    sizerate = float(sizerate) - 0.1
-
-def initial():
-    #jpgの変更処理
-    thread3 = threading.Thread(target=change_image)
-    thread3.start()
+    def speeddown(self):
+        global interval
+        interval = float(interval) + 0.1
 
 
+    def sizeup(self):
+        global sizerate
+        sizerate = float(sizerate) + 0.1
 
-def view_image():
-    global item, canvas
+    def sizedown(self):
+        global sizerate
+        sizerate = float(sizerate) - 0.1
+
+    def initial(self):
+        #jpgの変更処理
+        thread3 = threading.Thread(target=self.change_image)
+        thread3.start()
+
+
+
+    def view_image(self):
+        global item, canvas
  
-    root = tkinter.Tk()
-    root.title('jpg viewer')
-    root.geometry("1000x650")
+        self.root = tkinter.Tk()
+        self.root.title('jpg viewer')
+        self.root.geometry("1000x650")
 
-    button4 = tk.Button(root, text = '停止', command=suspend)
-    button4.grid(row=0, column=1)  
-    button4.place(x=930, y=50) 
+        button4 = tk.Button(self.root, text = '停止', command=self.suspend)
+        button4.grid(row=0, column=1)  
+        button4.place(x=930, y=50) 
 
-    button5 = tk.Button(root, text = '再開', command=resume)
-    button5.grid(row=0, column=1)  
-    button5.place(x=930, y=80) 
+        button5 = tk.Button(self.root, text = '再開', command=self.resume)
+        button5.grid(row=0, column=1)  
+        button5.place(x=930, y=80) 
 
-    button6 = tk.Button(root, text = '終了', command=quit)
-    button6.grid(row=0, column=1)  
-    button6.place(x=930, y=110) 
+        button6 = tk.Button(self.root, text = '終了', command=self.quit)
+        button6.grid(row=0, column=1)  
+        button6.place(x=930, y=110) 
 
-    button7 = tk.Button(root, text = '加速', command=speedup)
-    button7.grid(row=0, column=1)  
-    button7.place(x=930, y=140) 
+        button7 = tk.Button(self.root, text = '加速', command=self.speedup)
+        button7.grid(row=0, column=1)  
+        button7.place(x=930, y=140) 
 
-    button8 = tk.Button(root, text = '減速', command=speeddown)
-    button8.grid(row=0, column=1)  
-    button8.place(x=930, y=170) 
+        button8 = tk.Button(self.root, text = '減速', command=self.speeddown)
+        button8.grid(row=0, column=1)  
+        button8.place(x=930, y=170) 
 
-    button9 = tk.Button(root, text = '拡大', command=sizeup)
-    button9.grid(row=0, column=1)  
-    button9.place(x=930, y=200) 
+        button9 = tk.Button(self.root, text = '拡大', command=self.sizeup)
+        button9.grid(row=0, column=1)  
+        button9.place(x=930, y=200) 
 
-    button10 = tk.Button(root, text = '縮小', command=sizedown)
-    button10.grid(row=0, column=1)  
-    button10.place(x=930, y=230) 
+        button10 = tk.Button(self.root, text = '縮小', command=self.sizedown)
+        button10.grid(row=0, column=1)  
+        button10.place(x=930, y=230) 
 
-    button11 = tk.Button(root, text = '最初から', command=initial)
-    button11.grid(row=0, column=1)  
-    button11.place(x=930, y=260) 
+        button11 = tk.Button(self.root, text = '最初から', command=self.initial)
+        button11.grid(row=0, column=1)  
+        button11.place(x=930, y=260) 
 
 
-    root.mainloop()
+        self.root.mainloop()
  
  
-def change_image():
-    txt2 = tk.Entry(width=100)
-    txt2.place(x=90, y=600)
-    for n in filenames:
-        if suspend_flag == 1:
-            while(1):
-                time.sleep(1)
-                if suspend_flag == 0:
-                    break
-        img2 = Image.open(n)
-        before_x, before_y = img2.size[0], img2.size[1]
-        x = int(round(float(300 / float(before_y) * float(before_x))))
-        y = 300
-        img2.thumbnail((x*float(sizerate), y*float(sizerate)), Image.ANTIALIAS)
-        #img2 = img2.resize((900,600),Image.ANTIALIAS)
-        img2 = ImageTk.PhotoImage(img2)
-        canvas = tkinter.Canvas(bg = "white", width=900, height=600)
-        canvas.place(x=0, y=0)
-        item = canvas.create_image(30, 30, image=img2, anchor=tkinter.NW)
-        print("size")
-        print(sizerate)
-        print("int")
-        print(interval)
-        int_interval=float(interval)
-        time.sleep(int_interval) 
-        canvas.itemconfig(item,image=img2)
-        txt2.delete(0, tk.END)
-        txt2.insert(tk.END,n)
+    def change_image(self):
+        for n in filenames:
+            if self.suspend_flag == 1:
+                while(1):
+                    time.sleep(1)
+                    if self.suspend_flag == 0:
+                        break
+            img2 = Image.open(n)
+            before_x, before_y = img2.size[0], img2.size[1]
+            x = int(round(float(300 / float(before_y) * float(before_x))))
+            y = 300
+            img2.thumbnail((x*float(sizerate), y*float(sizerate)), Image.ANTIALIAS)
+            #img2 = img2.resize((900,600),Image.ANTIALIAS)
+            img2 = ImageTk.PhotoImage(img2)
+            canvas = tkinter.Canvas(bg = "white", width=900, height=600)
+            canvas.place(x=0, y=0)
+            item = canvas.create_image(30, 30, image=img2, anchor=tkinter.NW)
+            print("size")
+            print(sizerate)
+            print("int")
+            print(interval)
+            int_interval=float(interval)
+            time.sleep(int_interval) 
+            canvas.itemconfig(item,image=img2)
 
+    def quit(self):
+        self.root.destroy()
 
 root = Tk()  
 root.title("Image Viewer")  
 root.geometry("850x300") 
 image_gui(root)  
+
+s=sub_gui()
 txt2 = tkinter.Entry(width=10)
 txt2.place(x=10, y=30)
 txt2.insert(tkinter.END,"1.0")
@@ -238,9 +238,9 @@ root.mainloop()
 
 
 #jpg表示画面を表示
-thread1 = threading.Thread(target=view_image)
+thread1 = threading.Thread(target=s.view_image)
 thread1.start()
 
 #jpgの変更処理
-thread2 = threading.Thread(target=change_image)
+thread2 = threading.Thread(target=s.change_image)
 thread2.start()
